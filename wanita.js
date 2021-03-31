b63fd310cb2a45db5666719411a779e7dba32d51
Wuanitawanjiku <wanjikuwuanita@gmail.com>
2021-03-31 21:54:13 +0300

    first commit

--- wanita.js
+++ wanita.js
@@ -1,12 +1,29 @@
-// var children = 20;
-//  function numbers(){
-//   var girls = children-10;
-//   console.log(girls);
-//  }
-//   numbers()
-  var text = "outside";
-  function logIt(){
-      console.log(text);
-      var text = "inside"
+//Come up with 2 examples that distinguish between an object, a function, and a method
+
+//object
+let field={
+  counties:47,
+  name:"Kenya",
+  population:52.57
+}
+console.log("There are, "+ field.counties +" counties in "+ field.name +" with a population of "+
+field.population + " million citizens");
+
+/*function
+function x(a, b) {
+  var sum = a + b
+  return sum 
+}*/
+
+console.log((12, 13));
+
+//method
+var student ={
+  age:19,
+  gender:"female",
+  year:2021,
+  details:function(age, gender, year){
+      request.this()}
   }
-  logIt();
+  console.log("All students are of the age "+ student.age +" years "+", all of the "+student.gender+ " gender. "+
+  " Year is "+student.year)
